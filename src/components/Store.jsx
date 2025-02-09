22d50796d12d6bbdf52e238e1345a4302aeb6284
Nimrod Acosta <52327093+nimplay@users.noreply.github.com>
2025-02-09 09:36:23 -0400

    hide env variables

--- src/components/Store.jsx
+++ src/components/Store.jsx
@@ -1,4 +1,6 @@
 import { useState, useEffect } from "preact/hooks";
+const PUBLIC_API_URL = import.meta.env.PUBLIC_API_URL;
+const PUBLIC_PAYPAL_CHECKOUT_URL = import.meta.env.PUBLIC_PAYPAL_CHECKOUT_URL
 
 export default function Store() {
   const [products, setProducts] = useState([]);
